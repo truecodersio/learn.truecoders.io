ecc12f1ed1287517efca262a84e892586087512b
Cody Winton <cody.t.winton@gmail.com>
2018-03-15 18:01:44 -0500

    Updated Index Layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
 const IndexLayout = ({ children }) => (
-  <div style={{ margin: `0 auto`, padding: `0 1rem` }}>
-    <h1>TrueCoders</h1>
-    {children()}
+  <div>
+    <Helmet
+      title="TrueCoders"
+      meta={[
+        { name: "description", content: "TrueCoders" },
+        { name: "keywords", content: "sample, something" }
+      ]}
+    />
+    <div
+      style={{
+        margin: "0 auto",
+        padding: "0px 1.0875rem 1.45rem",
+        paddingTop: 24
+      }}
+    >
+      {children()}
+    </div>
   </div>
 );
+
+IndexLayout.propTypes = {
+  children: PropTypes.func
+};
 
 export default IndexLayout;
