181df89b1edbfe7ee28daffd7e0e6ba9f9742b7c
Cody Winton <cody.t.winton@gmail.com>
2018-03-15 17:17:21 -0500

    Added default page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
-import React from "react"
+import React, { Component } from "react";
+import Link from "gatsby-link";
 
-export default () => <div>Hello world!</div>
+class App extends Component {
+  render() {
+    return (
+      <div style={{ color: `#0093c9` }}>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <br />
+        <div>
+          <Link to="/page-2/">Link</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
