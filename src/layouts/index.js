5b05dceae910f3e19787af2dd8aae6fa702e85dc
Cody Winton <cody.t.winton@gmail.com>
2018-03-16 16:51:26 -0500

    Converted layout to query for site title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,45 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
-import Sidebar from "../components/Sidebar";
+import Sidebar from '../components/Sidebar'
 
-const sidebarSize = "200px"
+const sidebarSize = '200px'
 
-const IndexLayout = ({ children }) => (
+const IndexLayout = ({ children, data }) => (
   <div>
     <Helmet
-      title="TrueCoders"
+      title={data.site.siteMetadata.title}
       meta={[
-        { name: "description", content: "TrueCoders" },
-        { name: "keywords", content: "sample, something" }
+        { name: 'description', content: 'TrueCoders' },
+        { name: 'keywords', content: 'sample, something' },
       ]}
     />
     <Sidebar />
     <div
       style={{
-        marginLeft: "256px",
-        marginTop: "40px"
+        marginLeft: '256px',
+        marginTop: '40px',
       }}
     >
-      {" "}
-      {children()}{" "}
+      {' '}
+      {children()}{' '}
     </div>
   </div>
-);
+)
 
 IndexLayout.propTypes = {
-  children: PropTypes.func
-};
+  children: PropTypes.func,
+}
 
-export default IndexLayout;
+export default IndexLayout
+
+export const query = graphql`
+  query IndexLayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
